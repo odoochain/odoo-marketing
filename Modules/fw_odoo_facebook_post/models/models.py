8528d0e6bc2987d1245564059ca797725eccf3e6
yvan_lenir <yvan@frontware.co.th>
2022-05-12 15:39:03 +0700

    odoo_marketing

--- Modules/fw_odoo_facebook_post/models/models.py
+++ Modules/fw_odoo_facebook_post/models/models.py
@@ -12,19 +12,43 @@ class fw_odoo_facebook_post(models.Model):
    
     fb_page_id = fields.Many2many("facebook.page_id")
     msg = fields.Char()
+    image = fields.Char()
 
     
 
     def send_post(self):
         for i in range(len(self.fb_page_id)):
-            payload = {
-            'message' : self.msg,
-            'access_token' : self.fb_page_id[i].facebook_access_token
-            }
-            post_url = 'https://graph.facebook.com/{}/feed'.format(self.fb_page_id[i].page_id) 
-            r = requests.post(post_url, data=payload)
-            print(r.text) 
+            if not self.image:
+                payload = {
+                'message' : self.msg,
+                'access_token' : self.fb_page_id[i].facebook_access_token
+                }
+                post_url = 'https://graph.facebook.com/{}/feed'.format(self.fb_page_id[i].page_id) 
+                r = requests.post(post_url, data=payload)
+                print(r.text)
+            elif not self.msg:
+                image_url = 'https://graph.facebook.com/{}/photos'.format(self.fb_page_id[i].page_id)
+                image_location = self.image
+                img_payload = {
+                'url' : image_location,
+                'access_token': self.fb_page_id[i].facebook_access_token
+                }
+                r = requests.post(image_url, data=img_payload)
+                print(r.text) 
+            else : 
+                image_url = 'https://graph.facebook.com/{}/photos'.format(self.fb_page_id[i].page_id)
+                image_location = self.image
+                img_payload = {
+                'message' : self.msg,
+                'url' : image_location,
+                'access_token': self.fb_page_id[i].facebook_access_token
+                }
+                r = requests.post(image_url, data=img_payload)
+                print(r.text)
         return r
+    
+
+
 
 #on ne peut pas mettre deux fois le même post sur la même page
 
