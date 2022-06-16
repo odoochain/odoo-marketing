b562be3639be5ab44ea71612be8409af2a95119b
kpo <kpo@frontware.com>
2022-06-16 15:14:30 +0700

    [FIX] remove duplicate

--- Modules/fw_odoo_facebook_post/models/help.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-class facebook_help(models.Model):
-    _name="facebook.help"
-    _description="Facebook help"
-
-    name=fields.Char()
